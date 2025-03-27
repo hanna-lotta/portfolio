a4951f205d272b8dd4c4769cbe9f157a58de8858
Hanna Seldén <hanna_lotta@hotmail.com>
2025-03-27 17:07:54 +0100

    css layout

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -4,7 +4,7 @@ const Education = () => {
   return (
 	<div className="home">
 	  <h2>mina utbildningar</h2>
-	  <p></p>
+	  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur illo non, odio hic sit iure corporis velit suscipit libero at vitae adipisci maxime minus molestiae quod ab ad. Sed, necessitatibus!</p>
 	</div>
   )
 }
--- src/pages/Prodject.jsx
+++ src/pages/Prodject.jsx
@@ -5,6 +5,7 @@ const Prodject = () => {
 
 	  <div className="home">
 		<h2>mina projekt</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, reprehenderit id obcaecati ex nostrum voluptatem ea error, atque quam dolorem facere eum ullam fugit, repudiandae incidunt quibusdam delectus accusamus officia!</p>
 	  </div>
 	)
   }
