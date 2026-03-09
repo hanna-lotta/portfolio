cef8c2d67784b756bbe9ea94e11208465f761a0c
Hanna Selden <hanna_lotta@hotmail.com>
2026-03-09 12:10:26 +0100

    update mistake corrected

--- src/data/lia.js
+++ src/data/lia.js
@@ -3,7 +3,7 @@ export const lia = {
     title: "LIA",
     seeking: "LIA-praktik avklarad hos OrbisLinks:",
     period1: "🗓 17 november 2025 – 8 februari 2026",
-	seeking: "Andra LIA-praktik perioden under OrbisLinks:",
+	seeking2: "Andra LIA-praktik perioden under OrbisLinks:",
     period2: "🗓 9 mars – 29 maj 2026",
     between: "Mellanperioden (februari–mars) ägnas åt examensarbete, där samarbete med företag är möjligt."
   },
@@ -11,7 +11,7 @@ export const lia = {
     title: "Internship",
     seeking: "Internship completed at OrbisLinks:",
     period1: "🗓 November 17, 2025 – February 8, 2026",
-    seeking: "Second internship period at OrbisLinks:",
+    seeking2: "Second internship period at OrbisLinks:",
     period2: "🗓 March 9 – May 29, 2026",
     between: "The period in between (February–March) is dedicated to thesis work, where collaboration with companies is possible."
   }
