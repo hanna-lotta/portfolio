fe509bd7290d868ccc69d85db20d957de7e7d6e8
Hanna Selden <hanna_lotta@hotmail.com>
2026-03-09 12:21:24 +0100

    correktion laguages

--- src/data/myProfile.js
+++ src/data/myProfile.js
@@ -1,10 +1,10 @@
 
 const myProfile = {
 	sv: {
-	description: "Jag heter Hanna Seldén och jag studerar till Frontendutvecklare på NBI/Handelsakademin i Göteborg. Jag söker LIA praktik under perioden 17 november - 8 februari samt 9 mars - 29 maj. Jag har ett yrkesliv inom funktionsstöd bakom mig, där jag har arbetat med människor med neuropsykiatriska funktionshinder. Jag är nu redo för ett ny karriär inom Frontendutveckling!"
+	description: "Jag heter Hanna Seldén och jag studerar till Frontendutvecklare på NBI/Handelsakademin i Göteborg. Jag har ett yrkesliv inom funktionsstöd bakom mig, där jag har arbetat med människor med neuropsykiatriska funktionshinder. Jag är nu redo för ett ny karriär inom Frontend och Fullstackutveckling!"
 	},
 	eng: {
-	description: "My name is Hanna Seldén and I am studying to become a Frontend Developer at NBI/Handelsakademin in Gothenburg. I am looking for LIA internship during the periods November 17 - February 8 and March 9 - May 29. I have a professional background in disability support, where I have worked with people with neuropsychiatric disabilities. I am now ready for a new career in Frontend Development!"
+	description: "My name is Hanna Seldén and I am studying to become a Frontend Developer at NBI/Handelsakademin in Gothenburg. I have a professional background in disability support services, where I have worked with people with neuropsychiatric disabilities. I am now ready for a new career in Frontend and Fullstack Development!"
 	}
 }
 
